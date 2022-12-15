09466ef65b098b304e3bb4d3cc1d795e913deaba
Megan Byrd <meganbyrd22@gmail.com>
2022-12-15 12:20:10 -0600

    Add all const values for buttons

--- calc.js
+++ calc.js
@@ -1,8 +1,24 @@
 //add html elements
 const numberButtons = document.getElementsByClassName("numberButtons");
-const 1Button = document.getElementById("1Button")
-const operatorButtons = document.getElementsByClassName("operatorButtons");
+const Button1 = document.getElementById("Button1")
+const Button2 = document.getElementById("Button2")
+const Button3 = document.getElementById("Button3")
+const Button4 = document.getElementById("Button4")
+const Button5 = document.getElementById("Button5")
+const Button6 = document.getElementById("Button6")
+const Button7 = document.getElementById("Button7")
+const Button8 = document.getElementById("Button8")
+const Button9 = document.getElementById("Button9")
+const Button0 = document.getElementById("Button0")
 
+const operatorButtons = document.getElementsByClassName("operatorButtons");
+const addButton = document.getElementById("addButton");
+const subtractButton = document.getElementById("subtractButton");
+const multiplyButton = document.getElementById("multiplyButton");
+const divideButton = document.getElementById("divideButton");
+
+const equalButton = document.getElementById("equalButton");
+const clearButton = document.getElementById("clear");
 
 
 //addition function 
