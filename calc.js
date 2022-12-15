a730fe9289dcf2ab0d11cc23939a80288a274020
Megan Byrd <meganbyrd22@gmail.com>
2022-12-15 12:14:04 -0600

    Change button ids to ButtonX to not lead with a number Add comments to organize functions Add html elements as const in js

---
+++ calc.js
@@ -0,0 +1,17 @@
+//add html elements
+const numberButtons = document.getElementsByClassName("numberButtons");
+const 1Button = document.getElementById("1Button")
+const operatorButtons = document.getElementsByClassName("operatorButtons");
+
+
+
+//addition function 
+
+
+//subtraction function
+
+
+//multiplication function
+
+
+//division function 
